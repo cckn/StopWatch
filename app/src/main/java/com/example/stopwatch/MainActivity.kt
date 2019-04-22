63de48cfbe4f7eb9158fedfefc95153a5ace575f
cckn <cckn1985@gmail.com>
2019-04-22 21:15:54 +0900

    기본적인 기능구현 완료

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.stopwatch
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlin.concurrent.timer
@@ -14,15 +15,26 @@ class MainActivity : AppCompatActivity() {
 
     private var isRunning = false
 
+    private var lap = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         startButton.setOnClickListener() {
             isRunning = !isRunning
             if (isRunning)
                 start()
             else
                 pause()
+        }
+
+        lapTimeButton.setOnClickListener() {
+            recordLapTime()
+        }
+
+        resetButton.setOnClickListener {
+            reset()
         }
     }
 
@@ -45,5 +57,29 @@ class MainActivity : AppCompatActivity() {
         startButton.setImageResource(R.drawable.ic_play_arrow_black_24dp)
         timerTask?.cancel()
     }
+
+    private fun recordLapTime(): Unit {
+        val lapTime = this.time
+        val textView = TextView(this)
+        textView.text = "$lap LAP : ${lapTime / 100}.${lapTime % 100}"
+
+        lapLayout.addView(textView, 0)
+        lap++
+    }
+
+    private fun reset(): Unit {
+
+        timerTask?.cancel()
+        time = 0
+        isRunning = false
+        startButton.setImageResource(R.drawable.ic_play_arrow_black_24dp)
+        secTextView.text = "0"
+        milliTextView.text = "00"
+
+        lapLayout.removeAllViewsInLayout()
+        lap = 1
+        /* TODO: Reset Button을 눌렀을 때 표시되는 시간이 0으로 초기화 되지 않는 버그가 존재(약 30%확률) */
+    }
+
 }
 
