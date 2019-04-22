a4ebc019bf882c4a497c05ff2f7a35908e9f5762
cckn <cckn1985@gmail.com>
2019-04-22 20:53:17 +0900

    기본 UI 및 play, pause 기능 구현 완료

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -1,12 +1,49 @@
 package com.example.stopwatch
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.concurrent.timer
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var time = 0
+    private var timerTask: Timer? = null
+
+    private var isRunning = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startButton.setOnClickListener() {
+            isRunning = !isRunning
+            if (isRunning)
+                start()
+            else
+                pause()
+        }
+    }
+
+
+    private fun start(): Unit {
+        startButton.setImageResource(R.drawable.ic_pause_black_24dp)
+        timerTask = timer(period = 10) {
+            time++
+
+            val sec = time / 100
+            val milli = time % 100
+            runOnUiThread {
+                secTextView.text = "$sec"
+                milliTextView.text = "$milli"
+            }
+        }
+    }
+
+    private fun pause(): Unit {
+        startButton.setImageResource(R.drawable.ic_play_arrow_black_24dp)
+        timerTask?.cancel()
     }
 }
+
